8e3587d20263741fdd17efe1e782e5f8499a0a43
Farid <76972056+Faro08@users.noreply.github.com>
2021-02-22 01:13:01 -0300

    estilos

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,23 +1,24 @@
 import React from 'react';
 
+import './styles/Badge.css';
 import confLogo from '../images/badge-header.svg';
 
 class Badge extends React.Component {
     render(){
         return(
-            <div>
-                <div>
+            <div className="Badge">
+                <div className="Badge__header"> 
                     <img src={confLogo} alt="logo"/>
                 </div>
-                <div>
-                    <img src="https://www.gravatar.com/avatar?d=identicon" alt="avatar"/>
+                <div className="Badge__section-name">
+                    <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="avatar"/>
                     <h1>Farid<br/>Pasi</h1>
                 </div>
-                <div>
-                    <p>Frontend Developer</p>
-                    <p>@faridgp8</p>
-                    <p>Inibot NEA</p>
+                <div className="Badge__section-info">
+                    <h3>Frontend Developer</h3>
+                    <div>@faridgp8</div>
                 </div>
+                <div className="Badge__footer">#platziconf</div>
             </div>
         )
 
